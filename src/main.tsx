ded07acc8b09ca72faec3cb9723571007710ce71
Gazzty <tomasgustavo99@gmail.com>
2026-04-20 19:57:18 -0300

    Fix a la página de mapa

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,19 @@ import App from './App.tsx'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
 
+// Fix Leaflet default icon paths for Vite bundler
+import L from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
